a0ca602e0b972fbb0e1227efefbb2ec48e2fc4ed
Muhammadjon019 <githubb453@gmail.com>
2022-10-30 23:46:15 +0500

    mmm

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("Birinchi Sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+
+if a > b:
+    print("Birinchi son ikkinchi sondan katta")
+elif a < b:
+    print("Ikkinchi son birinchi sondan kichkina")
+else:
+    print("Ikki son teng")
---
+++ uchinchidars.py
@@ -0,0 +1,15 @@
+# a = int(input("Son: "))
+
+# for x in range(50):
+    # if a > 50:
+        # print("Kamolov")
+    # elif a == 50:
+        # print("Murodovich")
+    # else:
+        # print("Muhammadjon")
+        
+a = int(input("Birinchi son: "))
+b = int(input("Ikkinchi son: "))
+c=0
+for x in range(a < b):
+    print("javob")
